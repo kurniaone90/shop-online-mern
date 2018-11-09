dac495ef62dafd630abdb426f9b7f9db48bd75c9
Ady Kurniawan <kurni4one90@gmail.com>
2018-11-09 14:29:23 +0700

    Berhasil Materi 3

--- app.js
+++ app.js
@@ -1,12 +1,25 @@
-var express = require("express");
-var path 	= require("path");
+var express 	= require("express");
+var path 		= require("path");
+var mongoose 	= require("mongoose");
+var config 		= require("./config/database");
+
+
+// DB Connect
+// mongoose.connect("mongodb://localhost:27017/shop_online");
+mongoose.connect(config.database);
+
+var db  = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error :"));
+db.once("open", function() {
+	console.log("Connected to mongodb")
+});
 
 // Initial App
-var app = express();
+var app 		= express();
 
 // Setup View Engine
 app.set("views", path.join(__dirname, "views"));
-app.set("views engine", "ejs");
+app.set("view engine", "ejs");
 
 // Setup public folder
 app.use(express.static(path.join(__dirname, "public")));
@@ -14,7 +27,10 @@ app.use(express.static(path.join(__dirname, "public")));
 
 // Setup Ind
 app.get('/', function(req, res){
-	res.send("Welcome To Kambing");
+	// res.send("Welcome To Kambing");
+	res.render("index", {
+		title: "Home"
+	});
 });
 
 var port = 3000;
